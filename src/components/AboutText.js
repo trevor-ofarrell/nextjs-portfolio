0de39e4da7519e2af4ac773f82df22bb21e12a6f
Trevor O'Farrell <47125174+trevor-ofarrell@users.noreply.github.com>
2021-01-02 08:22:11 -0800

    Update AboutText.js

--- src/components/AboutText.js
+++ src/components/AboutText.js
@@ -76,7 +76,7 @@ export const AboutText = () => {
             </Grid>
             <Grid item xs={12} sm={12} md={12} lg={12}>
               <Typography className={classes.textbody}>
-              I’m a east Bay Area native who loves mountain biking, chess, the outdoors, JavaScript, and making beautiful websites. Currently I’m seeking full-time employment, and interested in anything in the full stack web development/frontend spectrum.
+              I’m a Bay Area native who loves mountain biking, chess, the outdoors, and making beautiful websites. Currently I’m seeking full-time employment, and interested in anything in the full stack web development/frontend spectrum.
               </Typography>
               <Typography className={classes.textbody}>
               Feel free to reach out with inquires about hiring me for freelancing/consulting as well.
