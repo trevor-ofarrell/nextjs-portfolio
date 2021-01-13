0c50d549657a4195126a8baabd65958ba7b2e549
trevor-ofarrell <trevorofarrell963@gmail.com>
2021-01-12 18:54:11 -0800

    testing out _app.js refactor

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,4 @@
-import React, {useState, useEffect} from "react";
-
+import React from "react";
 import Head from "next/head";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { FormspreeProvider } from '@formspree/react';
@@ -9,39 +8,6 @@ import './styles.css'
 // A note about Server Side Rendering w/ stylesheets & Material UI
 // https://material-ui.com/guides/server-rendering/#server-rendering
 // -
-
-function useIsMounted() {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  return mounted;
-}
-
-function PreventFlashOfUnstyledContent() {
-  const mounted = useIsMounted();
-
-  if (mounted) {
-    return null;
-  }
-
-  return (
-    <Head>
-      <style
-        id="preventFlashOfUnstyledContent"
-        dangerouslySetInnerHTML={{
-          __html: `*, *::before, *::after { transition: none !important; }`,
-        }}
-      />
-      <meta
-        name="viewport"
-        content="minimum-scale=0.100, initial-scale=1.00, width=device-width"
-      />
-    </Head>
-  );
-}
 
 export default function SaveApp(props) {
   const { Component, pageProps } = props;
@@ -66,7 +32,6 @@ export default function SaveApp(props) {
         </Head>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <PreventFlashOfUnstyledContent/>
         <Component {...pageProps} />
       </React.Fragment>
     </FormspreeProvider>
